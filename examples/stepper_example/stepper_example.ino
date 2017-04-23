f6a6e97f8d11ff75641d45e0640289f4beb49c14
Milo Knowles <mknowles@mit.edu>
2017-04-22 23:53:13 -0400

    Modify stepper example to include both stepper motors

--- examples/stepper_example/stepper_example.ino
+++ examples/stepper_example/stepper_example.ino
@@ -1,36 +1,55 @@
 /*
 Adafruit Arduino - Lesson 16. Stepper
 */
-
 #include <Stepper.h>
 
-int in1Pin = 12;
-int in2Pin = 11;
-int in3Pin = 10;
-int in4Pin = 9;
+// RIGHT MOTOR
+int R_in1Pin = 13; // the not PWM one
+int R_in2Pin = 12; // looks same for Adafruit Feather M0
+int R_in3Pin = 11;
+int R_in4Pin = 10;
 
-Stepper motor(512, in1Pin, in2Pin, in3Pin, in4Pin);  
+// LEFT MOTOR
+int L_in1Pin = 9; // the not PWM one
+int L_in2Pin = 6; 
+int L_in3Pin = 5;
+int L_in4Pin = A4;
 
-void setup()
-{
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
-  pinMode(in3Pin, OUTPUT);
-  pinMode(in4Pin, OUTPUT);
+Stepper left_motor(512, L_in1Pin, L_in2Pin, L_in3Pin, L_in4Pin);  
+Stepper right_motor(512, R_in1Pin, R_in2Pin, R_in3Pin, R_in4Pin); 
+
+void setup() {
+  
+  pinMode(R_in1Pin, OUTPUT);
+  pinMode(R_in2Pin, OUTPUT);
+  pinMode(R_in3Pin, OUTPUT);
+  pinMode(R_in4Pin, OUTPUT);
+  
+  pinMode(L_in1Pin, OUTPUT);
+  pinMode(L_in2Pin, OUTPUT);
+  pinMode(L_in3Pin, OUTPUT);
+  pinMode(L_in4Pin, OUTPUT);
 
   // this line is for Leonardo's, it delays the serial interface
   // until the terminal window is opened
   while (!Serial);
   
   Serial.begin(9600);
-  motor.setSpeed(20);
+
+  // set the speed of the motors
+  // probably want to keep this low so that motors don't draw too much power
+  left_motor.setSpeed(5);
+  right_motor.setSpeed(5);
 }
 
-void loop()
-{
+void loop() {
+  
   if (Serial.available())
   {
     int steps = Serial.parseInt();
-    motor.step(steps);
+    Serial.println(steps);
+    
+    left_motor.step(steps);
+    right_motor.step(steps);
   }
 }
